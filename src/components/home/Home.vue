f901a470f1dfc297e81d92e892b1a9de953ed77c
Arya Kusuma <arya@badr-interactive.com>
2017-10-03 09:34:46 +0700

    add layout header sidebar and footer

---
+++ src/components/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="home">
+    <div id="page-container" class="fade page-sidebar-fixed page-header-fixed">
+      <!-- begin breadcrumb -->
+      <ol class="breadcrumb pull-right">
+        <li><a href="javascript:;">Home</a></li>
+        <li><a href="javascript:;">Dashboard</a></li>
+        <li class="active">Dashboard</li>
+      </ol>
+      <!-- end breadcrumb -->
+      <!-- begin page-header -->
+      <h1 class="page-header">Dashboard <small>this is an example of dashboard page...</small></h1>
+      <!-- end page-header -->
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'home'
+}
+</script>
